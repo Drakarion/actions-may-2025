dd5b172198ea1afa9947a06e4d8ea5e132de98c2
Drakarion <emkanades.s1@gmail.com>
2025-08-07 00:28:34 +0000

    change s3

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable instance_type {}
 
 terraform {
   backend "s3" {
-    bucket = "kaizen-adileta"
+    bucket = "kaizen-emirs"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
